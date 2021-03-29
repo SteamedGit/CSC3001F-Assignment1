d423398bd8e9526a95cebfbea6b1a2439fbc0392
Tim <timothyhitge@gmail.com>
2021-03-29 16:55:00 +0200

    Now checks that registration is valid. Can send messages between clients via the server in a basic way.

--- src/Client.java
+++ src/Client.java
@@ -13,11 +13,12 @@ import java.util.Date;
  */
 public class Client
 {
-
     public static void main(String[] args) throws IOException, InterruptedException {
 
+        String otherClients = new String();
+
         if (args.length != 3) {
-                System.out.println("Usage: java QuoteClient <client port> <client name> <hostname>");
+                System.out.println("Usage: java Client <client port> <client name> <hostname>");
                 return;
         }
         
@@ -31,21 +32,26 @@ public class Client
         registerClient(socket, args[1], packet );
 
 
-        //Here we wait for confirmation that we are registered... Need to add more logic, maybe a loop.
+        //Here we wait for confirmation that we are registered... Need to add more logic, maybe put inside a function.
+        //Maybe resend the register client if there has no server response. We cannot proceed without being registered
         buf = new byte[1024];
-        DatagramPacket packet2 = new DatagramPacket(buf, buf.length, address, 4445);
-        packet2 = new DatagramPacket(buf, buf.length);
-        socket.receive(packet2);
+        packet = new DatagramPacket(buf, buf.length);
+        socket.receive(packet);
         String messageFromServer = new String(packet.getData(), packet.getOffset(), packet.getLength());
         String[] messageArray = messageFromServer.split("\n");
         
-        if(messageArray[1].equals("Register-Client-REC")) //TODO: check that the names match
+        if(messageArray[1].equals("Register-Client-OK") && messageArray[3].equals(args[1]))
         {
              System.out.println("We are registered.");
         }
+        else if(messageArray[1].equals("Register-Client-BAD"))
+        {
+            System.out.println("Please Re-register: ");
+            System.out.println(messageArray[3]);
+        }
 
         
-        //Gets the list of all other registered clients. Should have this in main loop later on...
+        //Gets the list of all other registered clients.
         //This tells us who we can message.
         getOtherClients(socket, args[1], packet);
 
@@ -53,10 +59,45 @@ public class Client
         packet = new DatagramPacket(buf, buf.length);
         socket.receive(packet);
 
-        // display response
         String received = new String(packet.getData(), packet.getOffset(), packet.getLength());
-        System.out.println(received);
-        
+        otherClients = received.split("\n")[3];
+        System.out.println("Available Clients: " + otherClients);
+
+
+        //Send and Receive Messages
+        Scanner input = new Scanner(System.in);
+        while(true)
+        {
+            System.out.println("Do you want to send a message(S) or receive a message(R) or quit?(Q)");
+            String sRQuit = input.nextLine();
+            System.out.println("");
+            if(sRQuit.toLowerCase().equals("s"))
+            {
+                
+                System.out.println("Who do you want to send a message to?");
+                String recipient = input.nextLine();
+                System.out.println("What is the message?");
+                String text = input.nextLine();
+                System.out.println("");
+                sendMessage(socket, recipient, text, packet);
+                continue;
+            }
+            if(sRQuit.toLowerCase().equals("quit") || sRQuit.toLowerCase().equals("q") )
+            {
+                break;
+            }
+            buf = new byte[1024];
+            packet = new DatagramPacket(buf, buf.length);
+            socket.receive(packet);
+            messageFromServer = new String(packet.getData(), packet.getOffset(), packet.getLength());
+            messageArray = messageFromServer.split("\n");
+            System.out.println("Message from " + messageArray[3] + ":");
+            System.out.println(messageArray[4]+"\n");
+        }
+      
+      
+
+        input.close();
         socket.close();
     }
 
@@ -113,7 +154,7 @@ public class Client
     private static void getOtherClients(DatagramSocket socket, String clientName, DatagramPacket packet) throws IOException
     {
         String chatProtocolVersion = "ChatTP v1.0\n";
-        String chatRequestType = "List-Clients\n";
+        String chatRequestType = "Get-Clients\n";
         DateFormat df = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
         String chatDate = df.format(new Date()) + "\n";
         //Add hash...
@@ -126,7 +167,42 @@ public class Client
         
         socket.send(packet);
 
-        //TODO: Change List-Clients to Get-Clients
+    }
+
+    /**
+     * Sends message for another client to the server. The server will pass the message on to the intended recipient.
+     * Example:
+     * 
+     * ChatTP v1.0
+     * Send-MSG-C
+     * 29-03-2021 00:30:45
+     * Client 2
+     * Hello Client 2!
+     * 
+     * 
+     * 
+     * @param socket
+     * @param recipient
+     * @param text
+     * @param packet
+     * @throws IOException
+     */
+    private static void sendMessage(DatagramSocket socket, String recipient, String text, DatagramPacket packet) throws IOException
+    {
+        String chatProtocolVersion = "ChatTP v1.0\n";
+        String chatRequestType = "Send-MSG-C\n";
+        DateFormat df = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+        String chatDate = df.format(new Date()) + "\n";
+        //Add hash...
+        String recip = recipient + "\n";
+        String body = text + "\n";
+
+        String msg = chatProtocolVersion + chatRequestType + chatDate + recip + body;
+        byte[] buf = new byte[1024];
+        buf = msg.getBytes();
+        packet.setData(buf);
+        
+        socket.send(packet);
     }
 
 }
--- src/ServerThread.java
+++ src/ServerThread.java
@@ -42,38 +42,75 @@ public class ServerThread extends Thread {
 
                 if (messageArray[1].equals("Register-Client"))
                 {
-                    List<ClientData> result = clientData.stream()
+                    List<ClientData> result1 = clientData.stream() //check if another client with the same name exists
                     .filter(a -> a.getName().equals(messageArray[3]))
                     .collect(Collectors.toList());
-                    if(result.size() == 0)
+
+                    List<ClientData> result2 = clientData.stream() //check if another client with the same inetaddress and port exists
+                    .filter(a -> a.getPort()==packet.getPort())
+                    .filter(a-> a.getAddress().equals(packet.getAddress()))
+                    .collect(Collectors.toList());
+                    if(result1.size() == 0 && result2.size() == 0)
                     {
                         clientData.add(new ClientData(packet.getAddress(), packet.getPort(), messageArray[3]));
+                        confirmRegister(socket, packet.getAddress(), packet.getPort(), messageArray[3], packet);
+                        System.out.println(">" + messageArray[3]+" is registered.\n");
                     }
-                    else
+                    if(result1.size() >0)
                     {
-                        System.out.println("Already registered.");
+                        if(result1.get(0).getAddress().equals(packet.getAddress()) && result1.get(0).getPort() == packet.getPort())
+                        {
+                            System.out.println("Already registered as " + result1.get(0).getName());
+                            confirmRegister(socket, packet.getAddress(), packet.getPort(), messageArray[3], packet);
+                        }
+                        else
+                        {
+                            String error = "Someone with this name is already registered. Register with a new name.";
+                            System.out.println(error);
+                            badRegister(socket, packet.getAddress(), packet.getPort(), error, packet);
+                        }
+                        
                     }
-                    System.out.println(messageArray[3]+" is registered.");
-                    confirmRegister(socket, packet.getAddress(), packet.getPort(), messageArray[3], packet);
+                    else if(result2.size() > 0)
+                    {
+                        
+                        if(!result2.get(0).getName().equals(messageArray[3]))
+                        {
+                            String error = "Someone else is registered at " + result2.get(0).getAddress() + " " + result2.get(0).getPort() + " as " + result2.get(0).getName();
+                            System.out.println(error);
+                            badRegister(socket, packet.getAddress(), packet.getPort(), error, packet);
+                        }
+                        
+                    }
+                    
+                    
                 }
 
-                else if(messageArray[1].equals("List-Clients"))
+                else if(messageArray[1].equals("Get-Clients"))
                 {
                     sendListOfClients(socket, packet.getAddress(), packet.getPort(), messageArray[3], packet, clientData);
                 }
 
+                else if(messageArray[1].equals("Send-MSG-C")) //TODO: Add confirmation to sender that server received message and get confirmation from recipient of receival
+                {
+                    List<ClientData> result1 = clientData.stream() 
+                    .filter(a -> a.getName().equals(messageArray[3]))
+                    .collect(Collectors.toList());
 
+                    List<ClientData> result2 = clientData.stream() 
+                    .filter(a -> a.getPort()==packet.getPort())
+                    .filter(a-> a.getAddress().equals(packet.getAddress()))
+                    .collect(Collectors.toList());
 
- 
-        // send the response to the client at "address" and "port"
+                    System.out.println("Message for " + messageArray[3] + ": " + messageArray[4]);
+                    if (result1.size()>0 && result2.size()>0)
+                    {
+                        System.out.println(result1.get(0).getName());
+                        message(socket, result1.get(0).getAddress(), result1.get(0).getPort(), result2.get(0).getName() , messageArray[4], packet);
+                    }
+                    
+                }
                
-        /*
-                InetAddress address = packet.getAddress();
-                int port = packet.getPort();
-                byte[] sendBuf = new byte[512];
-                sendBuf = ("Server: Message Received").getBytes();
-                packet = new DatagramPacket(sendBuf, sendBuf.length, address, port);
-                socket.send(packet); */
             } 
             catch (IOException e) 
             {
@@ -84,11 +121,11 @@ public class ServerThread extends Thread {
     }
 
    /**
-    * Sends a request back to the client confirming that it is registered.
+    * Sends a respond back to the client confirming that it is registered.
     * Example:
     * 
     * ChatTP v1.0
-    * Register-Client-REC
+    * Register-Client-OK
     * 29-03-2021 00:30:45
     * Client 1
     *
@@ -102,14 +139,50 @@ public class ServerThread extends Thread {
     protected void confirmRegister(DatagramSocket socket, InetAddress address, int port, String clientName, DatagramPacket packet) throws IOException
     {
         String chatProtocolVersion = "ChatTP v1.0\n";
-        String chatRequestType = "Register-Client-REC\n";
+        String chatRequestType = "Register-Client-OK\n";
         DateFormat df = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
         String chatDate = df.format(new Date()) + "\n";
         //Add hash...
         String body = clientName + "\n";
         String msg = chatProtocolVersion + chatRequestType + chatDate + body;
+        System.out.println(msg);
         byte[] buf = new byte[1024];
         buf = msg.getBytes();
+        packet.setAddress(address);
+        packet.setPort(port);
+        packet.setData(buf);
+        socket.send(packet);
+    }
+
+   /**
+    * Sends a response back to the client indicating that registration was unsuccessful.
+    * Example:
+    *
+    * ChatTP v1.0
+    * Register-Client-BAD
+    * 29-03-2021 00:30:45
+    * Someone with this name is already registered. Register with a new name.
+    *
+    * @param socket
+    * @param address
+    * @param port
+    * @param error
+    * @param packet
+    * @throws IOException
+    */
+    protected void badRegister (DatagramSocket socket, InetAddress address, int port, String error, DatagramPacket packet) throws IOException
+    {
+        String chatProtocolVersion = "ChatTP v1.0\n";
+        String chatRequestType = "Register-Client-BAD\n";
+        DateFormat df = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+        String chatDate = df.format(new Date()) + "\n";
+        //Add hash...
+        String body = error + "\n";
+        String msg = chatProtocolVersion + chatRequestType + chatDate + body;
+        byte[] buf = new byte[1024];
+        buf = msg.getBytes();
+        packet.setAddress(address);
+        packet.setPort(port);
         packet.setData(buf);
         socket.send(packet);
     }
@@ -134,7 +207,6 @@ public class ServerThread extends Thread {
      */
     protected void sendListOfClients(DatagramSocket socket, InetAddress address, int port, String clientName, DatagramPacket packet, List<ClientData> clientData) throws IOException
     {
-        //TODO: If the body string is empty, put a "No other clients" in it.
         String chatProtocolVersion = "ChatTP v1.0\n";
         String chatRequestType = "List-Of-Clients\n";
         DateFormat df = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
@@ -151,13 +223,58 @@ public class ServerThread extends Thread {
             }
             
         }
+        if(body.isEmpty())
+        {
+            body = "No other clients";
+        }
         body += "\n";
         String msg = chatProtocolVersion + chatRequestType + chatDate + body;
         byte[] buf = new byte[1024];
         buf = msg.getBytes();
+        packet.setAddress(address);
+        packet.setPort(port);
         packet.setData(buf);
         socket.send(packet);
         
+    }
+
+    /**
+     * Forwards a client's message to the intended recipient.
+     * 
+     * Example:
+     * 
+     * ChatTP v1.0
+     * Send-MSG-S
+     * 29-03-2021 00:30:45
+     * Client 1
+     * Hello Client 2!
+     * 
+     * 
+     * @param socket
+     * @param address
+     * @param port
+     * @param sender
+     * @param text
+     * @param packet
+     * @throws IOException
+     */
+    protected void message(DatagramSocket socket, InetAddress address, int port, String sender, String text, DatagramPacket packet) throws IOException
+    {
+        String chatProtocolVersion = "ChatTP v1.0\n";
+        String chatRequestType = "Send-MSG-S\n";
+        DateFormat df = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+        String chatDate = df.format(new Date()) + "\n";
+        //Add hash...
+        String sndr = sender + "\n";
+        String body = text + "\n";
+        String msg = chatProtocolVersion + chatRequestType + chatDate + sndr + body;
+        //System.out.println(msg);
+        byte[] buf = new byte[1024];
+        buf = msg.getBytes();
+        packet.setAddress(address);
+        packet.setPort(port);
+        packet.setData(buf);
+        socket.send(packet);
     }
  
 }
