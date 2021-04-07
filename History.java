4261aec27136927a3e77b14385f90f084b5dc40a
Ali <strali011@myuct.ac.za>
2021-04-07 11:56:56 +0200

    History class updated. Filename generator can handle more than 2 clients. Can fetch history from a textfile.

--- History.java
+++ History.java
@@ -1,7 +1,11 @@
 import java.io.File;  // Import the File class
 import java.io.FileWriter;
 import java.io.IOException;
-import java.net.DatagramPacket; 
+import java.net.DatagramPacket;
+import java.util.Collections; 
+import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList; 
 /**
  * Class for managing and updating successfully sent messages
  * between two clients.
@@ -27,14 +31,7 @@ public class History {
         client1 = m.getSender();
         client2 = m.getReciever();
 
-        //Doing this so that the same pair has the same file
-        //for chat updates
-        if(client1.compareTo(client2) > 1){
-            fileName = client1+""+client2+".txt";
-        }
-        else {
-            fileName = client2+""+client1+".txt";
-        }
+        String fileName = gen(client1, client2);
 
         try{
             File file = new File(fileName);
@@ -52,5 +49,60 @@ public class History {
             e.printStackTrace();
         }
 
+    }
+
+    /**
+     * Fetches chat history for c clients.
+     * @param c Variable number of clients.
+     */
+    public void fetch(String ... c){
+        String fileName = gen(c);
+        boolean exists = false;
+        File file = new File(fileName);
+        try{
+            if (file.createNewFile()){//if a new file was created, that means there's no history
+                System.out.println("No chat history exists for these users.");
+                file.delete(); //deletes file that was made for the testing
+            }
+            else{//if a file already exists, we know we can read from it
+                Scanner scFile = new Scanner(file);
+                //TODO: read in textfile, change "->" into ":" and print to terminal
+                while(scFile.hasNextLine()){
+                    String line = scFile.nextLine();
+                    String[] msg = line.split("->");
+                    System.out.println(msg[0] + ": " +msg[1]+"\n"); //Changes format from C1->Hi to C1: Hi
+                }
+            }
+
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+
+        
+    }
+
+    /**
+     * Generates a filename.
+     * @param c ... Accepts a variable number of client names
+     */
+    public String gen(String ... c){
+        List <String> clientList = new ArrayList<String>();
+        int count = 0;
+        String fileName = "";
+
+        //loading all clients into a list
+        for(String item: c){
+            clientList.add(item);
+            count ++;
+        }
+
+        Collections.sort(clientList); //Sorts client names in ascending order to match filename
+        for(String item: clientList){
+            fileName = ""+ fileName + item;
+        }
+
+        fileName = fileName+".txt";
+        return fileName;
     }
 }
